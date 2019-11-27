e0107ff09fd55e2fd5be8585536c155eadfba5e3
Jimmy Yeung <jiyeung@ebay.com>
2019-11-27 09:35:58 +0800

    Initial Commit

---
+++ swiftui-tertis/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-tertis
+//
+//  Created by Yeung, Jimmy(AWF) on 27/11/2019.
+//  Copyright © 2019 Jimmy Yeung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
